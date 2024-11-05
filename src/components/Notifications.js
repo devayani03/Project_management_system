80bb4e6a83ac712c3929b721999029263ad8a44e
SailorMoon <114327511+Anushka091922@users.noreply.github.com>
2024-11-05 12:20:17 +0530

    uploading notifications feature component

---
+++ src/components/Notifications.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Notification.css"; // Make sure to create this CSS file
+
+function Notification({ message, isVisible, onClose }) {
+  if (!isVisible) return null;
+
+  return (
+    <div className="notification">
+      <p>{message}</p>
+      <button className="close-button" onClick={onClose}>
+        Close
+      </button>
+    </div>
+  );
+}
+
+export default Notification;
