3a9a0bccaf37836248263bdb9877e9628ec52194
SailorMoon <114327511+Anushka091922@users.noreply.github.com>
2024-11-05 12:01:20 +0530

    Update Announcement.js
    Notification feature added to the code

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,18 +1,27 @@
 import { IconButton } from "@material-ui/core";
 import { Menu, MoreVert } from "@material-ui/icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Announcement.css";
+import Notification from "./Notification"; // Import Notification component
 
 function Announcement({ image, name, date, content, authorId }) {
+  const [notificationOpen, setNotificationOpen] = useState(false); // State for notification
+  const [notificationMessage, setNotificationMessage] = useState(""); // State for notification message
+
+  const handleNotificationClose = () => setNotificationOpen(false);
+
+  useEffect(() => {
+    // Trigger notification when a new announcement is received
+    setNotificationMessage("New announcement posted!");
+    setNotificationOpen(true);
+  }, [content]);
+
   return (
     <div className="announcement">
       <div className="announcement__informationContainer">
         <div className="announcement__infoSection">
           <div className="announcement__imageContainer">
-            <img
-              src="https://res.cloudinary.com/dyxnmjtrg/image/upload/v1730742456/pfp-1_bsmdwc.png"
-              alt="Profile photo"
-            />
+            <img src="https://res.cloudinary.com/dyxnmjtrg/image/upload/v1730742456/pfp-1_bsmdwc.png" alt="Profile photo" />
           </div>
           <div className="announcement__nameAndDate">
             <div className="announcement__name">{name}</div>
@@ -26,6 +35,13 @@ function Announcement({ image, name, date, content, authorId }) {
         </div>
       </div>
       <div className="announcement__content">{content}</div>
+
+      {/* Notification Component */}
+      <Notification
+        message={notificationMessage}
+        isVisible={notificationOpen}
+        onClose={handleNotificationClose}
+      />
     </div>
   );
 }
