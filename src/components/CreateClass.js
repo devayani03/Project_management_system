38eb8fc375a58cb56c559f0af466c4ac14947bff
Devayani <devayanichandane@gmail.com>
2024-11-22 01:16:19 +0530

    code generated in ui

--- src/components/CreateClass.js
+++ src/components/CreateClass.js
@@ -1,11 +1,11 @@
 import {
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogContentText,
-	DialogTitle,
-	TextField,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -14,104 +14,112 @@ import { auth, db } from "../firebase";
 import { createDialogAtom } from "../utils/atoms";
 
 function CreateClass() {
-	const [user, loading, error] = useAuthState(auth);
-	const [open, setOpen] = useRecoilState(createDialogAtom);
-	const [className, setClassName] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const [open, setOpen] = useRecoilState(createDialogAtom);
+  const [className, setClassName] = useState("");
+  const [classId, setClassId] = useState(""); // State to store the class ID
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const handleClose = () => {
+    setOpen(false);
+    setClassName("");
+    setClassId(""); // Reset class ID when the dialog is closed
+  };
 
-	const createClass = async () => {
-		try {
-			// Create a new class document
-			const newClass = await db.collection("classes").add({
-				creatorUid: user.uid,
-				name: className,
-				creatorName: user.displayName,
-				creatorPhoto: user.photoURL,
-				posts: [],
-			});
+  const createClass = async () => {
+    try {
+      // Create a new class document
+      const newClass = await db.collection("classes").add({
+        creatorUid: user.uid,
+        name: className,
+        creatorName: user.displayName,
+        creatorPhoto: user.photoURL,
+        posts: [],
+      });
 
-			// Log the class ID (document ID) to the console
-			console.log("Class ID:", newClass.id);
+      console.log("Class ID:", newClass.id);
+      setClassId(newClass.id); // Store the class ID in state
 
-			// Create a chat room for the new class
-			const chatRoom = await db.collection("chats").add({
-				classId: newClass.id,
-				className: className,
-				createdBy: user.uid,
-				members: [user.uid], // Add the class creator as a default member
-				messages: [], // Initialize an empty messages array or create a sub-collection for messages
-			});
+      // Create a chat room for the new class
+      const chatRoom = await db.collection("chats").add({
+        classId: newClass.id,
+        className: className,
+        createdBy: user.uid,
+        members: [user.uid],
+        messages: [],
+      });
 
-			console.log("Chat room created with ID:", chatRoom.id);
+      console.log("Chat room created with ID:", chatRoom.id);
 
-			// Update the class document to include the chat room ID
-			await db.collection("classes").doc(newClass.id).update({
-				chatRoomId: chatRoom.id,
-			});
+      // Update the class document to include the chat room ID
+      await db.collection("classes").doc(newClass.id).update({
+        chatRoomId: chatRoom.id,
+      });
 
-			// Add to current user's class list
-			const userRef = await db
-				.collection("users")
-				.where("uid", "==", user.uid)
-				.get();
-			const docId = userRef.docs[0].id;
-			const userData = userRef.docs[0].data();
-			let userClasses = userData.enrolledClassrooms || [];
-			userClasses.push({
-				id: newClass.id,
-				name: className,
-				creatorName: user.displayName,
-				creatorPhoto: user.photoURL,
-				chatRoomId: chatRoom.id,
-			});
-			const docRef = await db.collection("users").doc(docId);
-			await docRef.update({
-				enrolledClassrooms: userClasses,
-			});
+      // Add to current user's class list
+      const userRef = await db
+        .collection("users")
+        .where("uid", "==", user.uid)
+        .get();
+      const docId = userRef.docs[0].id;
+      const userData = userRef.docs[0].data();
+      let userClasses = userData.enrolledClassrooms || [];
+      userClasses.push({
+        id: newClass.id,
+        name: className,
+        creatorName: user.displayName,
+        creatorPhoto: user.photoURL,
+        chatRoomId: chatRoom.id,
+      });
+      const docRef = await db.collection("users").doc(docId);
+      await docRef.update({
+        enrolledClassrooms: userClasses,
+      });
 
-			handleClose();
-			alert("Classroom and chat room created successfully!");
-		} catch (err) {
-			alert(`Cannot create class - ${err.message}`);
-		}
-	};
+      alert("Classroom and chat room created successfully!");
+    } catch (err) {
+      alert(`Cannot create class - ${err.message}`);
+    }
+  };
 
-	return (
-		<div>
-			<Dialog
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="form-dialog-title"
-			>
-				<DialogTitle id="form-dialog-title">Create group</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						Enter the name of the group, and we will create a group for you!
-					</DialogContentText>
-					<TextField
-						autoFocus
-						margin="dense"
-						label="Group Name"
-						type="text"
-						fullWidth
-						value={className}
-						onChange={(e) => setClassName(e.target.value)}
-					/>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose} color="primary">
-						Cancel
-					</Button>
-					<Button onClick={createClass} color="primary">
-						Create
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</div>
-	);
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Create group</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter the name of the group, and we will create a group for you!
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Group Name"
+            type="text"
+            fullWidth
+            value={className}
+            onChange={(e) => setClassName(e.target.value)}
+          />
+          {/* Conditionally render the Class ID if it exists */}
+          {classId && (
+            <div style={{ marginTop: "10px" }}>
+              <strong>Class ID:</strong> {classId}
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={createClass} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default CreateClass;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,94 +11,98 @@ import "./Navbar.css";
 import { useHistory } from "react-router-dom";
 
 function Navbar() {
-	const history = useHistory(); // Move history declaration to the top
-	const [user, loading, error] = useAuthState(auth);
-	const [anchorEl, setAnchorEl] = useState(null);
-	const [createOpened, setCreateOpened] = useRecoilState(createDialogAtom);
-	const [joinOpened, setJoinOpened] = useRecoilState(joinDialogAtom);
+  const history = useHistory(); // Move history declaration to the top
+  const [user, loading, error] = useAuthState(auth);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [createOpened, setCreateOpened] = useRecoilState(createDialogAtom);
+  const [joinOpened, setJoinOpened] = useRecoilState(joinDialogAtom);
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	const openHome = () => {
-		history.push("/");
-	};
+  const openHome = () => {
+    history.push("/");
+  };
 
-	const openMeet = () => {
-		window.open("https://meet.google.com", "_blank");
-	};
+  const openMeet = () => {
+    window.open("https://meet.google.com", "_blank");
+  };
 
-	const openTextEditor = () => {
-		history.push("/editor");
-	};
+  const openTextEditor = () => {
+    history.push("/editor");
+  };
 
-	const openGoogleDocs = () => {
-		window.open("https://docs.google.com", "_blank");
-	};
+  const openGoogleDocs = () => {
+    window.open("https://docs.google.com", "_blank");
+  };
 
-	return (
-		<>
-			<CreateClass />
-			<JoinClass />
-			<nav className="navbar">
-				<div className="navbar__left">
-					<img
-						src="https://res.cloudinary.com/dyxnmjtrg/image/upload/v1729241256/book_logo_tvwpqi.png"
-						alt="logo"
-						className="home__image"
-					/>
-					<span>myProject</span>
-				</div>
-				<div className="navbar__right">
-					<IconButton onClick={openHome}>
-						<Home />
-					</IconButton>
-					<IconButton
-						aria-controls="simple-menu"
-						aria-haspopup="true"
-						onClick={handleClick}
-					>
-						<Add />
-					</IconButton>
-					<IconButton onClick={openMeet}>Meet</IconButton>
-					<IconButton onClick={openTextEditor}>Text Editor</IconButton>
-					<IconButton onClick={openGoogleDocs}>Google Docs</IconButton>
-					<IconButton onClick={logout}>
-						<Avatar src={user?.photoURL} />
-					</IconButton>
-					<Menu
-						id="simple-menu"
-						anchorEl={anchorEl}
-						keepMounted
-						open={Boolean(anchorEl)}
-						onClose={handleClose}
-					>
-						<MenuItem
-							onClick={() => {
-								setCreateOpened(true);
-								handleClose();
-							}}
-						>
-							Create group
-						</MenuItem>
-						<MenuItem
-							onClick={() => {
-								setJoinOpened(true);
-								handleClose();
-							}}
-						>
-							Join group
-						</MenuItem>
-					</Menu>
-				</div>
-			</nav>
-		</>
-	);
+  return (
+    <>
+      <CreateClass />
+      <JoinClass />
+      <nav className="navbar">
+        <div className="navbar__left">
+          <img
+            src="https://res.cloudinary.com/dyxnmjtrg/image/upload/v1729241256/book_logo_tvwpqi.png"
+            alt="logo"
+            className="home__image"
+            style={{ width: "80px", height: "60px" }}
+          />
+
+          <span style={{ fontWeight: "bold", color: "#00008B" }}>
+            MyProject
+          </span>
+        </div>
+        <div className="navbar__right">
+          <IconButton onClick={openHome}>
+            <Home />
+          </IconButton>
+          <IconButton
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <Add />
+          </IconButton>
+          <IconButton onClick={openMeet}>Meet</IconButton>
+          <IconButton onClick={openTextEditor}>Text Editor</IconButton>
+          <IconButton onClick={openGoogleDocs}>Google Docs</IconButton>
+          <IconButton onClick={logout}>
+            <Avatar src={user?.photoURL} />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem
+              onClick={() => {
+                setCreateOpened(true);
+                handleClose();
+              }}
+            >
+              Create group
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setJoinOpened(true);
+                handleClose();
+              }}
+            >
+              Join group
+            </MenuItem>
+          </Menu>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
